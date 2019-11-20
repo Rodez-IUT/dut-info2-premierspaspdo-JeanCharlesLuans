a6511b6a5ff9fdf54a82ca50f5722a8fae7c1aa9
JeanCharlesLuans <jean-charles.luans@iut-rodez.fr>
2019-11-20 11:01:04 +0100

    Correction partie 2

--- all_users.php
+++ all_users.php
@@ -1,53 +1,69 @@
-<?php 
-	
-	// Connection a la BD
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>All users</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        th, td {
+            padding: 8px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+    </style>
+</head>
+<body>
+
+<?php
+
 $host = 'localhost';
+$port = '8889';
 $db = 'my_activities';
 $user = 'root';
 $pass = 'root';
 $charset = 'utf8mb4';
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
 $options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
+    PDO::ATTR_EMULATE_PREPARES => false,
 ];
 try {
-     $pdo = new PDO($dsn, $user, $pass, $options);
+    $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (PDOException $e) {
-     throw new PDOException($e->getMessage(), (int)$e->getCode());
+    echo $e->getMessage() ;
+    throw new PDOException($e->getMessage(), (int)$e->getCode());
 }
 
- ?>
+?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>All User</title>
-</head>
-<body>
-	<td>
-		<tr> ID </tr>
-		<tr> USERNAME </tr>
-		<tr> EMAIL </tr>
-		<tr> STATUS </tr>
-	</td>
-	<td>
-		<?php
+<h1>All Users</h1>
 
-			$stmt = $pdo ->query('SELECT * FROM users JOIN status ON user.status_id = status.id;');
+<?php
+$stmt = $pdo->query('select users.id as user_id, username, email, s.name as status from users join status s on users.status_id = s.id');
+?>
+<table>
+    <tr>
+        <th>Id</th>
+        <th>Username</th>
+        <th>Email</th>
+        <th>Status</th>
+    </tr>
+    <?php while ($row = $stmt->fetch()) { ?>
+    <tr>
+        <td><?php echo $row['user_id']?></td>
+        <td><?php echo $row['username']?></td>
+        <td><?php echo $row['email']?></td>
+        <td><?php echo $row['status']?></td>
+    </tr>
+    <?php } ?>
+</table>
 
-			while ($row = $stmt->fetch()) {
-				echo "<tr>". $row['id'] ."</td>";
-				echo "<tr>". $row['username'] ."</td>";
-				echo "<tr>". $row['email'] ."</td>";
-				echo "<tr>". $row['name'] ."</td>";
-			}
-		 ?>
-
-
-	</td>
 
 </body>
 </html>
